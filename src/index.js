4dc63c835339b9ac01318228c0133d31276faa36
Christineeebagz <107945065+Christineeebagz@users.noreply.github.com>
2024-03-15 11:59:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Server is running on port ' + PORT));
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// const PORT = process.env.PORT || 3000;
+// server.listen(PORT, () => console.log('Server is running on port ' + PORT));
