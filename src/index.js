b4915b47b5d4e88754b95970b7dde6b65804223c
Christineeebagz <107945065+Christineeebagz@users.noreply.github.com>
2024-03-15 11:34:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// // const PORT = process.env.PORT || 3000;
+// // server.listen(PORT, () => console.log('Server is running on port ' + PORT));
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => console.log('Server is running on port ' + PORT));
