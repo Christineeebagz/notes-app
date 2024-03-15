4e6b52783b170cfd8119903030ee4feaffcd2714
Christineeebagz <107945065+Christineeebagz@users.noreply.github.com>
2024-03-15 10:54:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ root.render(
   </React.StrictMode>
 );
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(“Server is running on port 3000”));
+server.listen(PORT, () => console.log('Server is running on port ' + PORT));
